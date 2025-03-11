1afb0455d8b465141ecb77c5df3a16bac657ba46
dev soft <dev.soft687@gmail.com>
2025-03-12 01:34:51 +0530

    server and vercel

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
-
 const mongoDBConn = require('./config/mongoDbConfig');
-
-
 const router = require('./routers/routers');
 const app = express()
 app.use(express.json())
@@ -14,12 +11,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 // const port = 5000
-
-
 app.use('/api/v1', router);
-
 mongoDBConn()
-
 // app.listen(port, () => console.log(`🚀 Server running on http://localhost:${port}`));
-
 module.exports = app;
