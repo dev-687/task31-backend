45fd6e9e13e3e45ba7621d802a51d20eb6c4649e
dev soft <dev.soft687@gmail.com>
2025-03-13 02:39:15 +0530

    correct logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -88,6 +88,11 @@ exports.allUser= async (req,res)=>{
 
 /** Logout the current user */
 exports.logout= async (req,res)=>{
-  res.clearCookie('token', { path: '/' });
+  res.clearCookie("token", token, {
+    httpOnly: true,
+    secure: true, 
+    sameSite: "None", 
+    path: '/'
+  });
   res.status(200).json({ message: "Logged out successfully" });
 }
