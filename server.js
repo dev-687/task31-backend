5c4543756d184481bed2fafb1051858f9005290c
dev soft <dev.soft687@gmail.com>
2025-03-12 16:32:43 +0530

    cors

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const mongoDBConn = require('./config/mongoDbConfig');
 const router = require('./routers/routers');
 const app = express()
 app.use(express.json())
-// app.use(cors({
-//     origin: "https://task31-frontend-5qdm.vercel.app", 
-//     credentials: true
-// }))
+app.use(cors({
+    origin: "https://task31-frontend-5qdm.vercel.app", 
+    credentials: true
+}))
 
-app.use(
-    cors({
-      origin: "https://task31-frontend-5qdm.vercel.app", // Your frontend URL
-      credentials: true, // Allow cookies and authentication headers
-      methods: "GET,POST,PUT,DELETE",
-      allowedHeaders: "Content-Type,Authorization",
-    })
-  );
+// app.use(
+//     cors({
+//       origin: "https://task31-frontend-5qdm.vercel.app", // Your frontend URL
+//       credentials: true, // Allow cookies and authentication headers
+//       methods: "GET,POST,PUT,DELETE",
+//       allowedHeaders: "Content-Type,Authorization",
+//     })
+//   );
 app.use(cookieParser())
 // const port = 5000
 app.use('/api/v1', router);
